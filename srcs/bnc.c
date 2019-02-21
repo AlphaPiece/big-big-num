d9191711a46578fa0a838621a2c44140afe47dfb
Zexi Wang <twopieces0921@gmail.com>
2019-02-21 16:25:06 -0500

    modified title

--- srcs/bnc.c
+++ srcs/bnc.c
@@ -6,7 +6,7 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 12:23:26 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/02/21 12:27:08 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2019/02/21 16:23:38 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(void)
 {
 	ft_printf("bnc 1.0\n");
-	ft_printf("Created 21 Feb 2019, by AlphaPiece Studio.\n\n");
+	ft_printf("Created by AlphaPiece Studio.\n\n");
 	compute();
 	return (0);
 }
